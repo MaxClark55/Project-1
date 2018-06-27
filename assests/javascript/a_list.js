56558338e23646e0bfe3a56497a6f7daf4fb80d9
Deuce40Driver <thorntonbl1@gmail.com>
2018-06-26 22:27:44 -0400

    brians update

--- assests/javascript/a_list.js
+++ assests/javascript/a_list.js
@@ -1,74 +1,42 @@
-const selectedCuisine = ['Greek', 'Italian', 'Thai', 'American', 'Indian', 'Chinese', 'Japanese', 'Dessert'];
-const ingredientsArray = [];
-console.log(selectedCuisine);
-console.log(ingredientsArray);
-
+let tonightsChoice;
 let diningIn = false;
-let cuisinePlaces = function() {
-    let zomatoOption = $(document).attr("data-name");
-    const queryURL = "";
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response) {
-        console.log(response);
-        console.log(queryURL);
-        var results = response.data;
-        console.log(results);
-    });
-}
-//  let cuisineOptions = {
-    //  js for listing the selectedCuisine array
-//  }
-let availableRecipes = function() {
-    // API for Yummly goes here.
-    let yummlyOption = $(this).attr("data-name");
-    const queryURL = "http://api.yummly.com/v1/api/recipes?_app_id=app-id&_app_key=app-key&your _search_parameters";
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response){
-        console.log(response);
-        console.log(queryURL);
-        var results = response.data;
-        console.log(results);
-    });
+
+// function changeOfPlans()    
+function proteinRecipes() {
+    console.log(obj.recipes[i]);
+
 }
 
-var location = function() {
-    if (diningIn === true) {
-        return ingredientsArray;
-        cookAtHome();
-    } else {
-        return cuisinePlaces;
+function cookAtHome(protein) {
+    console.log(protein);
+    const queryURL = 'https://cors-anywhere.herokuapp.com/' +'http://food2fork.com/api/search?key=91a38b32cc4fa088106c6c76b28feb95&q=' + protein;
+    console.log(queryURL);
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+    var obj;
+    obj = JSON.parse(response);  
+    console.log(obj.recipes[0]);
+    for (let i = 0; i < 5; i++) {
+        console.log(obj.recipes[i]);
     }
-};
-// 
-// returns cuisine places here if dining out in the selection.
-// 
-function changeOfPlans() {
-    // toggles alternatives. button click used as a toggle to the other option.
-    // if (cuisinePlaces != true) {
-        //return cookAtHome;
-        //} else {
-            //do nothing
-        //}
-    //}    
+    }); 
 }
 
-function cookAtHome() {
-    // where we call Yummly for the ingredients and return recipes.
-    //let chosenCuisine == selectedCuisine;
-    //let chosenIngredients = user's input;
-    //chosenIngredients.push(ingredientsArray[]);
-    //const queryURL = "http://api.yummly.com/v1/api/recipes?_app_id=app-id&_app_key=app-key&your _search_parameters";
-    //$.ajax({
-        //url: queryURL,
-        //method: "GET"
-    //}).then(function(response){
-        //console.log(response);
-        //console.log(queryURL);
-        //var results = response.data;
-        //console.log(results);
-    //});
+function selectedMeat() {
+    var buttonsArray = document.querySelectorAll(".btn");
+    console.log(buttonsArray);
+    for (var i = 0; i < buttonsArray.length; i++) {
+        buttonsArray[i].addEventListener("click", function (e) {
+            console.log(e.target.id);
+            cookAtHome(e.target.id);
+        }, false); 
+    }
+    //When user clicks cuisine option from carousel, logs the option and stores in 
+    //variable "tonightsChoice"
+    // .onclick("button") {
+        // tonightsChoice = button.onclick("button")
+    // };
 }
+selectedMeat();
