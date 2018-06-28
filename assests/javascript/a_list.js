989e64aa5c779c3ebad96395c9bfe7addc483f9f
Deuce40Driver <thorntonbl1@gmail.com>
2018-06-27 23:43:25 -0400

    final submit

--- assests/javascript/a_list.js
+++ assests/javascript/a_list.js
@@ -1,42 +1,30 @@
-let tonightsChoice;
-let diningIn = false;
-
-// function changeOfPlans()    
-function proteinRecipes() {
-    console.log(obj.recipes[i]);
-
-}
-
+//Food2Fork API - working
 function cookAtHome(protein) {
-    console.log(protein);
     const queryURL = 'https://cors-anywhere.herokuapp.com/' +'http://food2fork.com/api/search?key=91a38b32cc4fa088106c6c76b28feb95&q=' + protein;
-    console.log(queryURL);
+    
     $.ajax({
         url: queryURL,
         method: "GET"
       }).then(function(response) {
-    var obj;
-    obj = JSON.parse(response);  
-    console.log(obj.recipes[0]);
-    for (let i = 0; i < 5; i++) {
-        console.log(obj.recipes[i]);
+        var obj;
+        obj = JSON.parse(response); 
+            for (let i = 0; i < 5; i++) {
+                let link = document.createElement("a");
+                link.textContent = obj.recipes[i].title;
+                link.href = obj.recipes[i].source_url;
+                document.querySelector("#recipes").appendChild(link);
+            }
+        }); 
     }
-    }); 
-}
 
+//Protein selector
 function selectedMeat() {
     var buttonsArray = document.querySelectorAll(".btn");
-    console.log(buttonsArray);
     for (var i = 0; i < buttonsArray.length; i++) {
         buttonsArray[i].addEventListener("click", function (e) {
-            console.log(e.target.id);
             cookAtHome(e.target.id);
         }, false); 
     }
-    //When user clicks cuisine option from carousel, logs the option and stores in 
-    //variable "tonightsChoice"
-    // .onclick("button") {
-        // tonightsChoice = button.onclick("button")
-    // };
 }
+
 selectedMeat();
